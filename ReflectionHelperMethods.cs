b48d85f4eb3b510d348744044828c984257d4f30
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2022-10-20 20:33:45 +0300

    Update ReflectionHelperMethods.cs

--- ReflectionHelperMethods.cs
+++ ReflectionHelperMethods.cs
@@ -1,10 +1,10 @@
 public class ReflectionHelperMethods
 {
-    private static object GetPrivateFieldValue(Type type, object controller, string fieldName)
+    private static object GetPrivateFieldValue(Type type, object obj, string fieldName)
     {
         BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
 
         FieldInfo field = type.GetField(fieldName, flags);
-        return field.GetValue(controller);
+        return field.GetValue(obj);
     }
 }
