e62721b093b9c152e1570dcee38d83208188da64
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2022-11-12 17:50:50 +0200

    Update ReflectionHelperMethods.cs

--- ReflectionHelperMethods.cs
+++ ReflectionHelperMethods.cs
@@ -15,4 +15,32 @@ public class ReflectionHelperMethods
 
         return type;
     }
+    private static object CreateObjectInstance(Type type, params object[] parameters)
+    {
+        try
+        {
+            var desiredConstructor = type.GetConstructors()
+                .FirstOrDefault(x => x.GetParameters().Any());
+
+            if (desiredConstructor == null)
+            {
+                return Activator.CreateInstance(type, parameters);
+            }
+
+            var instances = new List<object>();
+
+            foreach (var parameterInfo in desiredConstructor.GetParameters())
+            {
+                var currentInstance = Activator.CreateInstance(GetType(parameterInfo.Name.Substring(1)));
+
+                instances.Add(currentInstance);
+            }
+
+            return Activator.CreateInstance(type, instances.ToArray());
+        }
+        catch (TargetInvocationException e)
+        {
+            return e.InnerException.Message;
+        }
+    }
 }
